064bc358b39d9affcb79ea4b7c0a4414fe4d333b
Abiiarm <abiarmansyah.2021@student.uny.ac.id>
2024-05-20 07:10:23 +0700

    Update: Gas presentasi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BookHeartIcon, Facebook, Instagram, MapPinned, ShoppingCartIcon } from "lucide-react";
 import Header from "./components/Header";
 import Checkout from "./components/Checkout";
 import ProductList from "./features/producList/ProductList";
@@ -65,17 +66,28 @@ function App() {
         <footer className="bg-gray-800">
           <div className="container mx-auto max-w-7xl px-5 py-4 sm:px-6">
             <p className="text-center text-sm text-slate-200">
-              Hak Cipta <span className="text-center text-sm text-slate-200">@</span> oleh{" "}
+              Copyright <span className="text-center text-sm text-slate-200">@</span>{" "}
               <a href="https://github.com/Abiiarm" className="text-lime-500 transition duration-100 ease-in-out hover:text-lime-600" target="_blank" rel="noopener noreferrer" title="SixShop">
                 Kelompok 6
               </a>
+              <div className="mt-4 flex justify-center space-x-4">
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                  <Instagram strokeWidth={2.5} className="mx-4 w-6 stroke-gray-100" />
+                </a>
+                <a href="https://www.facebook.com//" target="_blank" rel="noopener noreferrer">
+                  <Facebook strokeWidth={2.5} className="mx-4 w-6 stroke-gray-100" />
+                </a>
+                <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer">
+                  <MapPinned strokeWidth={2.5} className="mx-4 w-6 stroke-gray-100" />
+                </a>
+              </div>
             </p>
-            <div className="mb-2 mt-2 text-center">
-              <a href="https://forms.gle/E1EtaSxY5Nfg7CYh6" target="_blank" rel="noopener noreferrer">
-                <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">Nilai Kami</button>
-              </a>
-            </div>
           </div>
+          {/* <div className="mb-2 mt-2 text-center">
+            <a href="https://forms.gle/E1EtaSxY5Nfg7CYh6" target="_blank" rel="noopener noreferrer">
+              <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">Nilai Kami</button>
+            </a>
+          </div> */}
         </footer>
       )}
       {modalStates.cart && <CartModal onClose={() => toggleModal("cart")} onCheckout={handleOpenCheckout} />}
